e2c5b5eeb3c487b753a059bbb002b0192c2bc271
syd <iamsyd.dev@gmail.com>
2022-07-15 22:01:47 -0700

    Score-Card ✅

---
+++ app/play-with-comp.js
@@ -0,0 +1,8 @@
+const rockBtn = document.querySelector('#rockBtn');
+const scissorBtn = document.querySelector('#scissorBtn');
+const paperBtn = document.querySelector('#paperBtn');
+const playAgainBtn = document.querySelector('#playAgainBtn');
+
+playAgainBtn.addEventListener('click', () => {
+console.log('lol')
+})
